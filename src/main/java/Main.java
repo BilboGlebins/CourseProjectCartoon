3b077f2e47625acd3c2a0b30c73fc41e75296b3b
BilboGlebins <glebkalton@mail.ru>
2023-08-17 14:30:02 +0700

    Construction of the structure, pictures and design of the main menu window commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.util.Objects;
+
+public class Main extends Application{
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("main-menu.fxml")));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.courseprojectcartoon {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.courseprojectcartoon to javafx.fxml;
-    exports com.example.courseprojectcartoon;
-}
